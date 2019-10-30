2e766aad88e607d733837fbae6597f559b472355
v1j1 <viji@womenwhocode.com>
2019-10-30 01:32:40 -0500

    fixed posting reports

--- test.py
+++ test.py
@@ -24,4 +24,7 @@ class TestHello(unittest.TestCase):
         self.assertIn("{name}", rv.data)
 
 if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
     unittest.main()
