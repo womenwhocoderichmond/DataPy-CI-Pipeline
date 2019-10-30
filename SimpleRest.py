818a5cdc2fad6aabbf97e5250b3e198e6987d7b9
Viji natarajan <vnatarajan@vcg-vna-mac.local>
2019-10-29 23:49:08 -0500

    initial revision

---
+++ SimpleRest.py
@@ -0,0 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/greet')
+def say_hello():
+  return 'Greetings from Flask Server'
+
+@app.route('/')
+def index():
+  return 'Home Page'
+
+@app.route('/hello')
+def hello():
+  return 'Hello, greetings from different endpoint'
+
+#adding variables
+@app.route('/hello/<username>')
+def show_user(username):
+  #returns the username
+  return 'Welcome: %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+  #returns the post, the post_id should be an int
+  return str(post_id)
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+import SimpleRest as app
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, 'Home Page')
+
+    def test_hello_greet(self):
+        rv = self.app.get('/greet')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, 'Greetings from Flask Server')
+
+    def test_hello_name(self):
+        name = 'Simon'
+        rv = self.app.get('/hello/{name}')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn("{name}", rv.data)
+
+if __name__ == '__main__':
+    unittest.main()
