d9fc8c56f40c0aa3f8b05feffad2555f5e9a07b6
v1j1 <viji@womenwhocode.com>
2020-07-08 22:58:18 -0400

    added linting

--- simple_rest.py
+++ simple_rest.py
@@ -1,28 +1,32 @@
 from flask import Flask
+"""Some Comment"""
 APP = Flask(__name__)
 
 
 @APP.route('/greet')
 def say_hello():
+    """Some Comment"""
     return 'Greetings from Flask Server'
 
 @APP.route('/')
 def index():
+    """Some Comment"""
     return 'Home Page'
 
 @APP.route('/hello')
 def hello():
+    """Some Comment"""
     return 'Hello, greetings from different endpoint'
 
 #adding variables
 @APP.route('/hello/<username>')
 def show_user(username):
-  #returns the username
+    """Some Comment"""
     return 'Welcome: %s' % username
 
 @APP.route('/post/<int:post_id>')
 def show_post(post_id):
-  #returns the post, the post_id should be an int
+    """returns the post, the post_id should be an int"""
     return str(post_id)
 
 if __name__ == '__main__':
